6a18ef945095fa30a2b77548e4c97b876c8aa77e
sathurjan <j.sathurjan@gmail.com>
2024-06-06 13:42:08 +0530

    simple jwt

--- AdsPrediction/models.py
+++ AdsPrediction/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Training Data Model
+class TrainingData(models.Model):
+    name=models.CharField(max_length=100)
+    data_url=models.URLField(max_length=200,unique=True)
+    
+    def __str__(self):
+        return self.name + " - " +self.data_url
--- DigitWebAds/settings.py
+++ DigitWebAds/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AdsPrediction.apps.AdspredictionConfig',
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
